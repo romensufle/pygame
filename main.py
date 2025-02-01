4dc36f2de4a77b4a08508bb0c382c1232f24cd7c
romensufle <roman@suftin.ru>
2025-02-01 23:24:26 +0300

    Joe can shoot

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import os
 import sys
 
 import pygame
+from pygame.time import Clock
 
 pygame.init()
-size = width, height = 500, 500
+size = width, height = 500, 620
+screen_rect = (0, 0, width, height)
 screen = pygame.display.set_mode(size)
-all_sprites = pygame.sprite.Group()
+cowboy_sprites = pygame.sprite.Group()
+bullet_sprites = pygame.sprite.Group()
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -29,22 +33,30 @@ def load_image(name, colorkey=None):
 class Cowboy(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
-        self.image = load_image('cowboy — копия.png')
+        self.image = load_image('cowboy.png')
 
         self.rect = self.image.get_rect()
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, group):
+    size = (20, 20)
+
+    def __init__(self, group, pos):
         super().__init__(group)
         self.image = load_image('bullet.png')
 
-        self.rect = self.image.get_rect()
+        self.rect = pygame.Rect(pos, Bullet.size)
+
+    def update(self, *args, **kwargs):
+        self.rect.top += 1
+        if not self.rect.colliderect(screen_rect):
+            self.kill()  # удаляет пулю, если она ушла за экран
 
 
-Joe = Cowboy(all_sprites)
-bul = Bullet(all_sprites)
-
+Joe = Cowboy(cowboy_sprites)
+clock = pygame.time.Clock()
+bullet_spid = pygame.USEREVENT + 1
+pygame.time.set_timer(bullet_spid, 10)
 
 running = True
 while running:
@@ -52,14 +64,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            Joe.rect.left -= 50
+            Joe.rect.left -= 100
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            Joe.rect.right += 50
-
-
+            Joe.rect.right += 100
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            pos = (Joe.rect.bottomleft[0] + 40, Joe.rect.bottomleft[1])
+            bul = Bullet(bullet_sprites, pos)
+        if event.type == bullet_spid:
+            bullet_sprites.update()
 
     screen.fill('#000000')
-    all_sprites.draw(screen)
-    all_sprites.update()
+    cowboy_sprites.draw(screen)
+    cowboy_sprites.update()
+    bullet_sprites.draw(screen)
     pygame.display.flip()
 pygame.quit()
